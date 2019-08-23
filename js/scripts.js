75c7cf82e0b2f1e8feb7fc9205ba76c41b07fc90
Tyra-hans <tyrahans17@gmail.com>
2019-08-23 11:38:05 +0300

    Add toggle to what we do icons

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#design-1").click(function() {
+      $("#design-icon").toggle("slow");
+      $("#design-p").toggle("slow");
+    });
+    $("#development").click(function() {
+        $("#development-1").toggle("slow");
+        $("#development-p").toggle("slow");
+    });
+    $("#product-management").click(function() {
+      $("#prod-management-1").toggle("slow");
+      $("#prod-p").toggle("slow");
+    });
+
+});
