0ed71d6edeee91ef3b5c4e1002450d201df25627
Tyra-hans <tyrahans17@gmail.com>
2019-08-25 10:27:38 +0300

    Add footer

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,33 @@ $(document).click(function() {
       $("#prod-p").toggle();
     });
 
+    $('.wrap').hover(function () {
+      $('.work-4').fadeIn("slow");
+      $('#work-4').toggle("slow");
+  });
+  $('.wrap1').hover(function () {
+    $('.work-3').toggle();
+    $('#work-3').toggle();
+});
+$('.wrap1').hover(function () {
+  $('.work-3').toggle();
+  $('#work-3').toggle();
+        $("#work-2").hover(function(){
+          $(".work2").toggle('slow');
+      });
+        $("#work-1").hover(function(){
+          $(".work1").toggle('slow');
+        });
+        $("#work-5").hover(function(){
+          $(".work5").toggle('slow');
+        });
+        $("#work-6").hover(function(){
+          $(".work6").toggle('slow');
+        });
+        $("#work-7").hover(function(){
+          $(".work7").toggle('slow');
+        });
+        $("#work-8").hover(function(){
+          $(".work8").toggle('slow');
+        });
 });
