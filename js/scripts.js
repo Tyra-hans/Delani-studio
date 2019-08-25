6f64dab33494207ad5321d22e61c52f2beab2103
Tyra-hans <tyrahans17@gmail.com>
2019-08-25 14:09:23 +0300

    ad jquery for poertfolio hover on images

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,39 @@
-$(document).click(function() {
-    $("#design-1").click(function() {
-      $("#design-p").toggle();
-      $("#design-icon").toggle();
-    });
-    $("#development").click(function() {
-        $("#development-p").toggle();
-        $("#development-1").toggle();
-    });
-    $("#product-management").click(function() {
-      $("#prod-management-1").toggle();
-      $("#prod-p").toggle();
-    });
-
-    $('.wrap').hover(function () {
-      $('.work-4').fadeIn("slow");
-      $('#work-4').toggle("slow");
+$(document).ready(function() {
+  $("#design-1").click(function() {
+    $("#design-p").toggle();
+    $("#design-icon").toggle();
   });
-  $('.wrap1').hover(function () {
-    $('.work-3').toggle();
-    $('#work-3').toggle();
-});
-$('.wrap1').hover(function () {
-  $('.work-3').toggle();
-  $('#work-3').toggle();
-        $("#work-2").hover(function(){
-          $(".work2").toggle('slow');
-      });
-        $("#work-1").hover(function(){
-          $(".work1").toggle('slow');
-        });
-        $("#work-5").hover(function(){
-          $(".work5").toggle('slow');
-        });
-        $("#work-6").hover(function(){
-          $(".work6").toggle('slow');
-        });
-        $("#work-7").hover(function(){
-          $(".work7").toggle('slow');
-        });
-        $("#work-8").hover(function(){
-          $(".work8").toggle('slow');
-        });
+  $("#development").click(function() {
+    $("#development-p").toggle();
+    $("#development-1").toggle();
+  });
+  $("#product-management").click(function() {
+    $("#prod-management-1").toggle();
+    $("#prod-p").toggle();
+  });
+
+  $("#work4").hover(function(){
+    $(".work4").toggle('slow');
+  });
+  $("#work3").hover(function(){
+    $(".work3").toggle('slow');
+  });
+  $("#work2").hover(function(){
+    $(".work2").toggle('slow');
+  });
+  $("#work1").hover(function(){
+    $(".work1").toggle('slow');
+  });
+  $("#work5").hover(function(){
+    $(".work5").toggle('slow');
+  });
+  $("#work6").hover(function(){
+    $(".work6").toggle('slow');
+  });
+  $("#work7").hover(function(){
+    $(".work7").toggle('slow');
+  });
+  $("#work8").hover(function(){
+    $(".work8").toggle('slow');
+  });
 });
