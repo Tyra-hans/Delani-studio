ec13c838bec82040f072b1a4bbe8f5a8938f5713
Tyra-hans <tyrahans17@gmail.com>
2019-08-26 08:02:33 +0300

    add final corrections on indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     $("#prod-management-1").toggle();
     $("#prod-p").toggle();
   });
-// portfolio section
+  // portfolio section
   $("#work4").hover(function(){
     $(".work4").toggle(500);
   });
@@ -39,27 +39,27 @@ $(document).ready(function() {
   });
 });
 
-  // getting the name from form
-  $(document).ready(function() {
+// getting the name from form
+$(document).ready(function() {
   $("#submit").click (function(event) {
-  console.log("working")
-  event.preventDefault();
-  var name=document.getElementById("name").value;
-  var email=document.getElementById("email").value;
-  var text=document.getElementById("text").value;
-  // form validation
-  if (name == ""){
-  alert("please enter your name");
-  return false;
-}else if(email == ""){
-  alert("please enter your email")
-  return false;
-}else if(text == ""){
-  alert("please tell us something")
-  return false;
-}else {
-  alert(name + " your message is received. Thank you");
-}
+    console.log("working")
+    event.preventDefault();
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var text=document.getElementById("text").value;
+    // form validation
+    if (name == ""){
+      alert("please enter your name");
+      return false;
+    }else if(email == ""){
+      alert("please enter your email")
+      return false;
+    }else if(text == ""){
+      alert("please tell us something")
+      return false;
+    }else {
+      alert(name + " your message is received. Thank you");
+    }
 
-});
+  });
 });
